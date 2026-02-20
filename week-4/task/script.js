b699d22a85c3a96bb3e7bb3c1752b4d61c552902
Clove Twilight <clovetwilight3@outlook.com>
2026-02-20 10:47:54 +0000

    Does it work like... this?

--- week-4/task/script.js
+++ week-4/task/script.js
@@ -1,11 +1,10 @@
-function hide() {
-    document.querySelector(".cookies").style.display = "none";
-}
-
 document.getElementById("decline").addEventListener("click", function() {
-    hide();
+    document.querySelector(".cookies").style.display = "none";
 });
 
 document.getElementById("consent").addEventListener("click", function() {
     document.querySelector(".cookies h3").textContent = "Congratulations";
+    document.querySelector(".cookies p").textContent = "Visit Skyrim now and we will set a dragon on you free of charge!";
+    document.getElementById("consent").style.display = "none";
+    document.getElementById("decline").textContent = "Yay!";
 });
