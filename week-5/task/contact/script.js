4253b72f8da8b8d1c6166419631b098758608d01
Clove Twilight <clovetwilight3@outlook.com>
2026-02-20 11:53:59 +0000

    hmm console it out rq

--- week-5/task/contact/script.js
+++ week-5/task/contact/script.js
@@ -3,3 +3,15 @@ document.getElementById("submit").addEventListener("click", function(event) {
     document.querySelector(".thanks").style.display = "block";
     event.preventDefault();
 });
+
+let email = document.forms["form"]["email"].value;
+let query = document.forms["form"]["query"].value;
+let name = document.forms["form"]["name"].value;
+let message = document.forms["form"]["message"].value;
+
+console.log("New contact form submission:");
+console.log("Name: " + name);
+console.log("Email: " + email);
+console.log("Query: " + query);
+console.log("Message: " + message);
+
