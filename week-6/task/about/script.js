548a618b780c9a211ae76ffb65e86e5def7d4c5d
Clove Twilight <clovetwilight3@outlook.com>
2026-02-27 10:15:15 +0000

    test somehing a little off topic

--- week-6/task/about/script.js
+++ week-6/task/about/script.js
@@ -1,33 +1,30 @@
-document.getElementById("places").addEventListener('click', plbtn => {
-    const xhttp = new XMLHttpRequest();
+const display = document.getElementById("display");
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const data = JSON.parse(this.responseText);
-            const placesList = document.createElement('ul');
-            data.forEach(place => {
-                const placeItem = document.createElement('li');
-                placeItem.textContent = `${place.name} - ${place.location}`;
-                placesList.appendChild(placeItem);
-            });
-            document.body.appendChild(placesList);
-        }
-    };
+function renderList(items, formatter) {
+  display.innerHTML = "";
+  const ul = document.createElement("ul");
+  items.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = formatter(item);
+    ul.appendChild(li);
+  });
+  display.appendChild(ul);
+}
 
-    xhttp.open("GET", "/COM419/week-6/task/about/data/places.json", true);
-    xhttp.send();
+document.getElementById("places").addEventListener("click", () => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const data = JSON.parse(this.responseText);
+      renderList(data, place => `${place.name} - ${place.location}`);
+    }
+  };
+  xhttp.open("GET", "/COM419/week-6/task/about/data/places.json", true);
+  xhttp.send();
 });
 
-document.getElementById("creatures").addEventListener('click', crbtn => {
-    fetch('/COM419/week-6/task/about/data/creatures.json')
-        .then(response => response.json())
-        .then(data => {
-            const creaturesList = document.createElement('ul');
-            data.forEach(creature => {
-                const creatureItem = document.createElement('li');
-                creatureItem.textContent = `${creature.name} - ${creature.description}`;
-                creaturesList.appendChild(creatureItem);
-            });
-            document.body.appendChild(creaturesList);
-        });
+document.getElementById("creatures").addEventListener("click", () => {
+  fetch("/COM419/week-6/task/about/data/creatures.json")
+    .then(response => response.json())
+    .then(data => renderList(data, creature => `${creature.name} - ${creature.description}`));
 });
