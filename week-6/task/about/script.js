3133edd892e998a7935e4cb84dcc93c2f1329ac7
Clove Twilight <clovetwilight3@outlook.com>
2026-02-27 10:07:29 +0000

    test 1

---
+++ week-6/task/about/script.js
@@ -0,0 +1,33 @@
+document.getElementById("places").addEventListener('click', plbtn => {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const data = JSON.parse(this.responseText);
+            const placesList = document.createElement('ul');
+            data.forEach(place => {
+                const placeItem = document.createElement('li');
+                placeItem.textContent = `${place.name} - ${place.description}`;
+                placesList.appendChild(placeItem);
+            });
+            document.body.appendChild(placesList);
+        }
+    };
+
+    xhttp.open("GET", "/COM419/week-6/task/about/data/places.json", true);
+    xhttp.send();
+});
+
+document.getElementById("creatures").addEventListener('click', crbtn => {
+    fetch('/COM419/week-6/task/about/data/creatures.json')
+        .then(response => response.json())
+        .then(data => {
+            const creaturesList = document.createElement('ul');
+            data.forEach(creature => {
+                const creatureItem = document.createElement('li');
+                creatureItem.textContent = `${creature.name} - ${creature.description}`;
+                creaturesList.appendChild(creatureItem);
+            });
+            document.body.appendChild(creaturesList);
+        });
+});
